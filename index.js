8654902d4db1f01aab91dfc4c33e4e7356d64828
Mo Wahid <97093583+thatswahid@users.noreply.github.com>
2022-05-24 15:08:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function sreach(){
+let filter = document.getElementById("name").value.toUpperCase();
+// console.log(nm);
+let myTable = document.getElementById("table");
+let tr = myTable.getElementsByTagName('tr');
+for(var i=0;i<tr.length;i++){
+    let td = tr[i].getElementsByTagName("td")[0];
+
+    if(td){
+        let textValue = td.textContent || td.innerHTML;
+
+        if(textValue.toUpperCase().indexOf(filter)>-1){
+            tr[i].style.display = "";
+        }else{
+            tr[i].style.display = "none";
+        }
+    }
+}
+}
